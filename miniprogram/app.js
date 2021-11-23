cc2078f67a5f5ffa76e13cb1d9c1cfc6f84fffed
aecra <admin@aecra.cn>
2021-11-23 15:01:32 +0800

    增加获取用户全局信息功能

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,5 +1,33 @@
 // app.js
 App({
+  globalData: {
+    // 用户信息
+    hasUserInfo: false,
+    userInfo: {
+      _id: "",
+      _openid: "",
+      openid: "",
+      defaultaddr: ["竹园1号楼", "一层", "1-101"],
+      name: "",
+      qq: "",
+      registerTime: null,
+      student_id: null,
+      phone: null
+    }
+  },
+
+  LoadInfo: async function () {
+    let result = await wx.cloud.callFunction({
+      name: 'userinfo'
+    });
+    result = result.result;
+
+    if (result.hasUserInfo) {
+      this.globalData.hasUserInfo = true;
+      this.globalData.userInfo = result.userInfo;
+    }
+  },
+
   onLaunch: function () {
     if (!wx.cloud) {
       console.error('请使用 2.2.3 或以上的基础库以使用云能力');
@@ -9,5 +37,7 @@ App({
         traceUser: true,
       });
     }
+
+    this.LoadInfo();
   }
 });
