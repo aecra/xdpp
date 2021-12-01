3ae36298b0dfc98c83920bc242ede92b9a32cca3
aecra <admin@aecra.cn>
2021-12-01 17:37:35 +0800

    新增云函数 packagekind

---
+++ cloudfunctions/packagekind/index.js
@@ -0,0 +1,23 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+
+cloud.init()
+
+// 云函数入口函数
+exports.main = async (event, context) => {
+    if (!cloud) {
+        console.error('请使用 2.2.3 或以上的基础库以使用云能力');
+    } else {
+        cloud.init({
+            env: 'cloud1-9g3jxgcr2ffa5389',
+        });
+    }
+
+    const db = cloud.database();
+    const addr = db.collection('packagekind');
+    let resAddr = await addr.get();
+
+    return {
+        result: resAddr
+    }
+}
