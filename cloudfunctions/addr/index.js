fba08505c7e03ad72b9e8b0586bfbd0bfbedbb3c
aecra <admin@aecra.cn>
2021-12-01 17:16:58 +0800

    新增函数 addr

---
+++ cloudfunctions/addr/index.js
@@ -0,0 +1,20 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+
+// 云函数入口函数
+exports.main = async (event, context) => {
+    if (!cloud) {
+        console.error('请使用 2.2.3 或以上的基础库以使用云能力');
+    } else {
+        cloud.init({
+            env: 'cloud1-9g3jxgcr2ffa5389',
+        });
+    }
+
+    const db = cloud.database();
+    const addr = db.collection('addr');
+    let resAddr = await addr.get();
+    return {
+        result: resAddr
+    }
+}
