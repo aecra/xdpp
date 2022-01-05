094a06707a449ee6300af919b58461336833bed8
aecra <admin@aecra.cn>
2022-01-05 10:29:51 +0800

    修复：发布订阅模型

--- miniprogram/utils/event.js
+++ miniprogram/utils/event.js
@@ -1,5 +1,6 @@
 class Event {
-  /** on 方法把订阅者所想要订阅的事件及相应的回调函数记录在 Event 对象的 cbs 属性中并调用 */
+  /** on 方法把订阅者所想要订阅的事件及相应的回调函数
+   * 记录在 Event 对象的 cbs 属性中并调用 */
   on(event, fn) {
     if (typeof fn !== 'function') {
       // eslint-disable-next-line no-console
@@ -10,14 +11,13 @@ class Event {
     this.data = this.data || {};
     (this.cbs[event] = this.cbs[event] || []).push(fn);
     if (this.data[event]) {
-      const callbacks = this.cbs[event];
-      callbacks?.forEach((callback) => {
-        callback(this.data[event]);
-      });
+      fn(this.data[event]);
     }
   }
 
-  /** emit 方法接受一个事件名称参数，在 Event 对象的 cbs 属性中取出对应的数组，并逐个执行里面的回调函数 */
+  /** emit 方法接受一个事件名称参数，
+   * 在 Event 对象的 cbs 属性中取出对应的数组，
+   * 并逐个执行里面的回调函数 */
   emit(event, data) {
     this.cbs = this.cbs || {};
     this.data = this.data || {};
@@ -28,32 +28,34 @@ class Event {
     });
   }
 
-  /** off 方法接受事件名称和当初注册的回调函数作参数，在 Event 对象的 cbs 属性中删除对应的回调函数。 */
+  /** off 方法接受事件名称和当初注册的回调函数作参数，
+   * 在 Event 对象的 cbs 属性中删除对应的回调函数。 */
   off(event, fn) {
     this.cbs = this.cbs || {};
     this.data = this.data || {};
     // all
     if (!arguments.length) {
-      this.cbs = {};
+      delete this.cbs;
+      delete this.data;
       return;
     }
-    const callbacks = this.cbs[event];
-    if (!callbacks) return;
-    const data = this.data[event];
-    if (!data) return;
-    // remove all handlers
     if (arguments.length === 1) {
-      delete this.cbs[event];
-      delete this.data[event];
+      if (this.cbs[event]) delete this.cbs[event];
+      if (this.data[event]) delete this.data[event];
       return;
     }
-    // remove specific handler
-    let cb;
-    for (let i = 0, len = callbacks.length; i < len; i += 1) {
-      cb = callbacks[i];
-      if (cb === fn || cb.fn === fn) {
-        callbacks.splice(i, 1);
-        break;
+    if (arguments.length === 2) {
+      const data = this.data[event];
+      if (!data) return;
+      const callbacks = this.cbs[event];
+      if (!callbacks) return;
+
+      for (let i = 0, len = callbacks.length; i < len; i += 1) {
+        const cb = callbacks[i];
+        if (cb === fn || cb.fn === fn) {
+          callbacks.splice(i, 1);
+          break;
+        }
       }
     }
   }
